8d63d88380ef4a6fd1e3dd0fafe1776f22683221
Filipe Colla <173160383+collafilipe@users.noreply.github.com>
2024-06-18 09:30:37 -0300

    Finished

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/DwVQgRck/imagem-2024-05-14-151403328.png',
+    title: "Service Desk",
+    desc: "This project posed a challenge wherein our team undertook the task of developing a service desk utilizing Type, JavaScript, and React. Within this context, I emerged as one of the primary contributors to the backend development.",
+    link: "https://github.com/Sync-FATEC/API-NEFRO/tree/main",
+    git: "https://github.com/Sync-FATEC/API-DESK",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/gkMFLqyZ/imagem-2024-05-14-150753345.png',
+    title: "Nefro API",
+    desc: "This was my debut academic project, where our team was challenged to create a website for mothers advocating for chronic kidney disease awareness in Brazil. I played a key role in backend development, focusing on login and blog functionalities.",
+    link: "https://github.com/Sync-FATEC/API-NEFRO/tree/main",
+    git: "https://github.com/Sync-FATEC/API-NEFRO/tree/main",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/fLfRfvgD/Captura-de-tela-20240515-093835.png',
+    title: "Personal Portfolio",
+    desc: "This project was a personal initiative to create a portfolio website. I was responsible for the entire development process, from design to deployment. The website was built using React, Tailwind CSS, and AOS animations.",
+    link: "https://portifolio-filipecolla.netlify.app",
+    git: "https://github.com/filipecolla/Portfolio",
+  }
+  
+];
