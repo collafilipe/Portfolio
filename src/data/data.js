959cd00a319ace0c6ab3400a208ef0b1b304f284
Filipe Colla <filipe.francisco@autaza.com>
2025-05-23 11:51:16 -0300

    Add new projects to portfolio: FAPG Transparency Portal and Tecsus Environmental Monitoring

--- src/data/data.js
+++ src/data/data.js
@@ -22,6 +22,21 @@ export const data = [
     desc: "This project was a personal initiative to create a portfolio website. I was responsible for the entire development process, from design to deployment. The website was built using React, Tailwind CSS, and AOS animations.",
     link: "https://portfoliofilipecolla.netlify.app",
     git: "https://github.com/collafilipe/Portfolio",
+  },
+  {
+    id: 4,
+    img:'https://i.postimg.cc/63VympjS/Captura-de-tela-2025-05-23-114531.png',
+    title: 'FAPG - Transparency portal',
+    desc: "Project developed by third-semester Multiplatform Software Development students for the API program, aiming to create a transparency portal for a foundation. It includes project data entry, filtered search, legacy system data migration, and graphical statistics with filters.",
+    link: "https://github.com/collafilipe/FAPG-Site-de-transparencia",
+    git: "https://github.com/collafilipe/FAPG-Site-de-transparencia",
+  },
+  {
+    id: 5,
+    img: 'https://i.postimg.cc/Jhbgyfdm/Captura-de-tela-2025-05-23-114436.png',
+    title: 'Tecsus - Environmental monitoring',
+    desc: "Project developed for the Tecsus company, focusing on environmental monitoring solutions. The project involved creating a web application for real-time data visualization and analysis.",
+    link: "https://github.com/collafilipe/TECSUS-Monitoramento-Ambiental",
+    git: "https://github.com/collafilipe/TECSUS-Monitoramento-Ambiental",
   }
-  
 ];
